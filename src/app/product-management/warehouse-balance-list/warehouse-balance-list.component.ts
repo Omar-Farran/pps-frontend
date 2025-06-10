a6cb9d85b003f843f26678013d6c5fc0aa98e79f
Izzeldeen <ezzeldeen.kalbouneh@arrowapp.net>
2025-06-10 13:27:24 +0300

    change product autocomplete in warehousebalance list

--- src/app/product-management/warehouse-balance-list/warehouse-balance-list.component.ts
+++ src/app/product-management/warehouse-balance-list/warehouse-balance-list.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { SelectItem } from 'src/app/data/select-item';
 import { BaseService } from 'src/app/shared/services/base.service';
 import { DirectionService } from 'src/app/shared/services/change-language.service';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-warehouse-balance-list',
@@ -20,11 +22,13 @@ export class WarehouseBalanceListComponent {
   ];
   locale:string;
     actionList: any[] = [];
+    searchProduct:string;
 products:any[];
 branchies:any[];
 sections:any;
 warehouses:any;
 dataSource: any[] = [];
+filteredProducts:SelectItem[];
   totalCount: number = 0
   id: number = null
   public searchForm = new FormGroup
@@ -33,7 +37,8 @@ dataSource: any[] = [];
       warehouseId:new FormControl(null),
       branchId:new FormControl(null),
       productId:new FormControl(null),
-      sectionId:new FormControl(null)
+      sectionId:new FormControl(null),
+      product:new FormControl(null)
     }
   )
   baseSearch = 
@@ -49,7 +54,8 @@ dataSource: any[] = [];
   constructor 
   ( 
     private baseService: BaseService,
-    public directionService: DirectionService
+    public directionService: DirectionService,
+    private translate: TranslateService
   ) 
   {}
   ngOnInit() : void 
@@ -60,6 +66,8 @@ dataSource: any[] = [];
     this.getBranchies();
     this.getWarehouses();
     this.locale =  this.directionService.getCurrentLanguage();
+    this.searchProduct = this.translate.instant('transaction.seasrch-product')
+
   }
   //#region Getters
   private getList () 
@@ -83,7 +91,9 @@ dataSource: any[] = [];
     if(form){
       this.baseSearch.branchId = form.branchId;
       this.baseSearch.warehouseId = form.warehouseId;
-      this.baseSearch.productId = form.productId;
+      if(form.product){
+      this.baseSearch.productId = form.product.id;
+      }
     }
       
       this.baseSearch.pageNumber = 0;
@@ -146,4 +156,19 @@ this.baseService.Get('WarehouseSections' , 'GetSectionsByWarehouseId/' + warehou
       this.searchForm.reset();
       this.onSearch();
      }
+
+      filterProducts(event: any) {
+         const query = event.query.toLowerCase();
+         this.getSelectItemList(query);
+         // this.filteredProducts = this.products.filter(product =>
+         //   product.name.toLowerCase().includes(query)
+         // );
+       }
+     
+       getSelectItemList(query){
+         
+         this.baseService.Get('Product' , 'GetSelectItemsList?query=' + query ).subscribe(res => {
+           this.filteredProducts = res as SelectItem[];
+         })
+       }
 }
